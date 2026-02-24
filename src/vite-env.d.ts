46f5b07bb273989f68bb9f8655e1a71c81668ae2
yanggeorge <alenym@qq.com>
2026-02-24 11:08:49 +0800

    vite-env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,4 @@
-declare module "*.scss" {
-  const content: { [className: string]: string };
-  export default content;
-}
-
-declare module "*.svg" {
-  const content: string;
-  export default content;
-}
+/// <reference types="vite/client" />
 
 declare module "*.svg?react" {
   import * as React from "react";
