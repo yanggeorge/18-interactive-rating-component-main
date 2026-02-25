10f960bbe300f1b1ccfe208a1da0bee7dec48b86
yanggeorge <alenym@qq.com>
2026-02-25 22:05:21 +0800

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
 
 import { useAtom } from "jotai";
-import { DevTools } from "jotai-devtools";
 
 import { scoreAtom } from "./atoms";
 import Card from "./components/Card";
 import Final from "./components/Final";
+import JotaiDevTools from "./utils/jotai-devtools";
 
 function App() {
   const [score, _] = useAtom(scoreAtom);
   console.log("🚀 ~ App.tsx:11 ~ App ~ score:", score);
   return (
     <>
-      <DevTools />
+      <JotaiDevTools />
       <main className="flex-1 grid place-items-center bg-black">
         {score === null && <Card />}
         {score && <Final />}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,18 @@
+import { useAtom } from "jotai";
+import { useState } from "react";
+
 import IconStart from "@/assets/icons/icon-star.svg?react";
+import { scoreAtom } from "@/atoms";
+import { cn } from "@/utils/cn";
+
 const Card = () => {
+  const [_, setScore] = useAtom(scoreAtom);
+  const [value, setValue] = useState(0);
+
+  const handleSubmit = () => {
+    setScore(value);
+  };
+
   return (
     <div className="w-81.75 h-90 p-6 flex flex-col gap-6 items-start bg-grey-950 rounded-2xl">
       <div className="rounded-full bg-grey-900 h-10 w-10 grid place-items-center">
@@ -17,16 +30,33 @@ const Card = () => {
             return (
               <button
                 key={item}
-                className="grid place-items-center h-10.5 w-10.5 rounded-full bg-grey-900 text-grey-500 text-preset5-bold"
+                className={cn(
+                  // 1. 基础布局：固定宽高、圆角、居中、字体
+                  "flex items-center justify-center h-10.5 w-10.5 rounded-full text-preset5-bold transition-colors cursor-pointer",
+
+                  // 2. 视觉修正：消除行高、间距干扰，并向下微调 2px (pt-0.5)
+                  "leading-none tracking-normal pt-0.5",
+
+                  // 3. 状态颜色：根据是否选中切换
+                  item === value
+                    ? "bg-white text-grey-900"
+                    : "bg-grey-900 text-grey-500 hover:bg-orange-500 hover:text-grey-900",
+                )}
+                onClick={() => {
+                  setValue(item);
+                }}
               >
-                {item}
+                <span>{item}</span>
               </button>
             );
           })}
         </div>
       </div>
 
-      <button className="w-full h-11.25 p-4 grid place-items-center bg-orange-500 text-grey-950 text-preset5-bold rounded-3xl">
+      <button
+        className="w-full h-11.25 p-4 grid place-items-center bg-orange-500 text-grey-950 text-preset5-bold rounded-3xl cursor-pointer"
+        onClick={handleSubmit}
+      >
         Submit
       </button>
     </div>
