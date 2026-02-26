2b600a242719db79c7fdc75ef30f611317c0002a
yanggeorge <alenym@qq.com>
2026-02-26 18:08:59 +0800

    md size

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,18 +16,20 @@ const Card = () => {
   };
 
   return (
-    <div className="w-81.75 h-90 p-6 flex flex-col gap-6 items-start bg-grey-950 rounded-2xl">
-      <div className="rounded-full bg-grey-900 h-10 w-10 grid place-items-center">
+    <div className="w-81.75 h-90 p-6 flex flex-col gap-6 md:gap-8 items-start bg-grey-950 rounded-2xl md:w-103 md:h-104 md:py-8">
+      <div className="rounded-full bg-grey-900 h-10 w-10 md:h-12 md:w-12 grid place-items-center">
         <IconStart />
       </div>
 
       <div className="flex flex-col gap-4">
-        <h1 className="text-white typography-2">How did we do?</h1>
-        <p className="text-grey-500 typography-5-reg">
+        <h1 className="text-white typography-2 md:typography-1">
+          How did we do?
+        </h1>
+        <p className="text-grey-500 typography-5-reg md:typography-4-reg">
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
-        <div className="flex gap-4 self-center">
+        <div className="flex gap-4 self-center md:gap-6">
           {[1, 2, 3, 4, 5].map((item) => {
             return (
               <button
@@ -38,6 +40,7 @@ const Card = () => {
 
                   // 2. 视觉修正：消除行高、间距干扰，并向下微调 2px (pt-0.5)
                   "leading-none tracking-normal pt-0.5",
+                  "md:h-12.75 md:w-12.75 md:typography-3",
 
                   // 3. 状态颜色：根据是否选中切换
                   item === value
@@ -62,7 +65,7 @@ const Card = () => {
         )}
         onClick={handleSubmit}
       >
-        Submit
+        SUBMIT
       </button>
     </div>
   );
--- src/components/Final.tsx
+++ src/components/Final.tsx
@@ -6,16 +6,18 @@ import { scoreAtom } from "@/atoms";
 const Final = () => {
   const [score, _] = useAtom(scoreAtom);
   return (
-    <div className="w-81.75 h-90 p-6 flex flex-col gap-6 items-center bg-grey-950 rounded-2xl">
+    <div className="w-81.75 h-90 p-6 flex flex-col gap-6 items-center bg-grey-950 rounded-2xl md:w-103 md:h-104 md:px-8 md:py-10 md:gap-8">
       <div>
         <IconThankYou />
       </div>
-      <div className="w-42 h-8 rounded-[22.5px] bg-grey-900 flex justify-center items-center typography-5-reg text-orange-500">
+      <div className="w-42 h-8 rounded-[22.5px] bg-grey-900 flex justify-center items-center typography-5-reg text-orange-500 leading-none md:w-48.25 md:typography-4-reg">
         You selected {score} out of 5
       </div>
       <div className="flex flex-col items-center gap-4">
-        <div className="typography-2 text-white">Thank you!</div>
-        <div className="typography-5-reg text-grey-500 text-center">
+        <div className="typography-2 text-white md:typography-1">
+          Thank you!
+        </div>
+        <div className="typography-5-reg text-grey-500 text-center md:typography-4-reg">
           We appreciate you taking the time to give a rating. If you ever need
           more support, don’t hesitate to get in touch!
         </div>
